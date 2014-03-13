f9ec7bee2420158ec400d40ee9fd97590b46cffc
WildBamaBoy <dustya@otelco.net>
2014-03-13 18:27:37 -0500

    Documenting constants

--- Minecraft/1.7.2/src/main/java/spiderqueen/core/Constants.java
+++ Minecraft/1.7.2/src/main/java/spiderqueen/core/Constants.java
@@ -2,9 +2,13 @@ package spiderqueen.core;
 
 public final class Constants 
 {
+	/** Will be beginning version. 
+	 *  -0.7.0.Alpha + minor fixes
+	 *  -0.8.0.Beta  + minor fixes
+	 *  -0.9.0.PreRelease + minor fixes
+	 *  -1.0.0.*/
 	public static final String VERSION = "0.7.0.Alpha";
-	
-	public static final float PI_F = 3.14159265F;
+
 	private Constants()
 	{
 	}
