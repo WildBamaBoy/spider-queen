706342be35e0334c86eca53814114641174c308e
WildBamaBoy <dustya@otelco.net>
2014-04-16 14:04:27 -0500

    Cocoons can now drop eggs

--- Minecraft/1.7.2/src/main/java/spiderqueen/entity/EntityCocoon.java
+++ Minecraft/1.7.2/src/main/java/spiderqueen/entity/EntityCocoon.java
@@ -199,8 +199,16 @@ public class EntityCocoon extends EntityCreature implements IEntityAdditionalSpa
 
 			if (!worldObj.isRemote)
 			{
+				final boolean doDropEgg = LogicHelper.getBooleanWithProbability(15);
+				final int dropAmount = LogicHelper.getNumberInRange(1, 2);
+				
 				entityDropItem(new ItemStack(SpiderQueen.getInstance().itemWeb, LogicHelper.getNumberInRange(0, 5), 0), 0);
-
+				
+				if (doDropEgg)
+				{
+					entityDropItem(new ItemStack(SpiderQueen.getInstance().itemSpiderEgg, dropAmount, 0), 0);
+				}
+				
 				try
 				{
 					worldObj.playSoundAtEntity(this, cocoonType.getDeathSound(), getSoundVolume(), getSoundPitch());
