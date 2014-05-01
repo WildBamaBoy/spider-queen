4b83bba7ab473655a071354daa35ce8cbe699fc1
WildBamaBoy <dustya@otelco.net>
2014-05-01 15:54:37 -0500

    Webs are now climbable

--- Minecraft/1.7.2/src/main/java/spiderqueen/blocks/BlockWeb.java
+++ Minecraft/1.7.2/src/main/java/spiderqueen/blocks/BlockWeb.java
@@ -6,35 +6,38 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.monster.EntitySpider;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import spiderqueen.core.SpiderQueen;
+import spiderqueen.entity.EntityHatchedSpider;
 
 public class BlockWeb extends Block
 {	
-    public BlockWeb() 
-    {
-		super(Material.plants);
+	public BlockWeb() 
+	{
+		super(Material.circuits);
 	}
-	
-    public int quantityDropped(Random random)
-    {
-        return 3;
-    }
-	
+
+	public int quantityDropped(Random random)
+	{
+		return 0;
+	}
+
 	public void checkForBed(World world, int x, int y, int z, int itr)
 	{
 		Block ww = SpiderQueen.getInstance().blockWeb;
 		Block ll = Blocks.log;
-		
+
 		if(world.getBlock(x,y,z) != ww) { return; }
-		
+
 		int cc = 0;
 		int wc = 0;
-		
+
 		if(world.getBlock(x-1,y,z-1) == ww) { cc++; }
 		if(world.getBlock(x-1,y,z) == ww) { cc++; }
 		if(world.getBlock(x-1,y,z+1) == ww) { cc++; }
@@ -44,7 +47,7 @@ public class BlockWeb extends Block
 		if(world.getBlock(x+1,y,z-1) == ww) { cc++; }
 		if(world.getBlock(x+1,y,z) == ww) { cc++; }
 		if(world.getBlock(x+1,y,z+1) == ww) { cc++; }
-		
+
 		if(world.getBlock(x-2,y,z-2) == ll) { wc++; }
 		if(world.getBlock(x-2,y,z-1) == ll) { wc++; }
 		if(world.getBlock(x-2,y,z) == ll) { wc++; }
@@ -65,7 +68,7 @@ public class BlockWeb extends Block
 		if(world.getBlock(x,y,z+2) == ll) { wc++; }
 		if(world.getBlock(x+1,y,z+2) == ll) { wc++; }
 		if(world.getBlock(x+2,y,z+2) == ll) { wc++; }
-		
+
 		if(cc == 9 & wc == 20)
 		{
 			world.setBlock(x-1,y,z-1,SpiderQueen.getInstance().blockWebBed);
@@ -93,59 +96,61 @@ public class BlockWeb extends Block
 			}
 		}
 	}
-	
-    public void onBlockAdded(World world, int i, int j, int k)
-    {
+
+	public void onBlockAdded(World world, int i, int j, int k)
+	{
 		checkForBed(world,i,j,k,0);
-    }
-	
-    public void onEntityCollidedWithBlock(World world, int i, int j, int k, Entity entity)
-    {
-		if(entity instanceof EntitySpider) { return; }
-		//if(entity instanceof EntityFriendlySpider) { return; }	//TODO
-		if(entity instanceof EntityPlayer) { return; }
-//		if(entity instanceof EntityFly)								//TODO
-//		{
-//			EntityFly el = (EntityFly)entity;
-//			el.setCocooned(true);
-//		}
-																	//TODO
-//		if(entity instanceof EntityGhast)
-//		{
-//			EntityGhast gg = (EntityGhast)entity;
-//			gg.webHit++;
-//			if(gg.webHit > 2)
-//			{
-//				entity.isInWeb = true;
-//			}
-//			return;
-//		}
-//		
-//        entity.isInWeb = true;
-		
-		entity.motionX = entity.motionX * -0.1D;
-		entity.motionZ = entity.motionZ * -0.1D;
-		entity.motionY = entity.motionY * 0.1D;
-    }
+	}
 
-    public boolean isOpaqueCube()
-    {
-        return false;
-    }
+	public void onEntityCollidedWithBlock(World world, int i, int j, int k, Entity entity)
+	{
+		if (entity instanceof EntitySpider || entity instanceof EntityHatchedSpider || entity instanceof EntityPlayer) 
+		{ 
+			return; 
+		}
 
-    public AxisAlignedBB getCollisionBoundingBoxFromPool(World world, int i, int j, int k)
-    {
-        return null;
-    }
+		else
+		{
+			entity.setInWeb();
 
-    public int getRenderType()
-    {
-        return 1;
-    }
-    
+			entity.motionX = entity.motionX * -0.1D;
+			entity.motionZ = entity.motionZ * -0.1D;
+			entity.motionY = entity.motionY * 0.1D;
+		}
+	}
+
+	public boolean isOpaqueCube()
+	{
+		return false;
+	}
+
+	public AxisAlignedBB getCollisionBoundingBoxFromPool(World world, int i, int j, int k)
+	{
+		return null;
+	}
+
+	public int getRenderType()
+	{
+		return 1;
+	}
+
 	@Override
 	public void registerBlockIcons(IIconRegister IIconRegister)
 	{
 		blockIcon = IIconRegister.registerIcon("spiderqueen:Web");
 	}
+
+	@Override
+	public boolean isLadder(IBlockAccess world, int x, int y, int z, EntityLivingBase entity) 
+	{
+		if (entity instanceof EntityPlayer || entity instanceof EntitySpider || entity instanceof EntityHatchedSpider)
+		{
+			return true;
+		}
+		
+		else
+		{
+			return false;
+		}
+	}	
 }
