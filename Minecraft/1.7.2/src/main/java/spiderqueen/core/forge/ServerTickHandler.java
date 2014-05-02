7ceacd4f7aa267b375afb812f9b69f48f09c3537
WildBamaBoy <dustya@otelco.net>
2014-05-02 03:01:18 -0500

    Player now has night vision

--- Minecraft/1.7.2/src/main/java/spiderqueen/core/forge/ServerTickHandler.java
+++ Minecraft/1.7.2/src/main/java/spiderqueen/core/forge/ServerTickHandler.java
@@ -15,6 +15,8 @@ import java.util.List;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.WorldServer;
 import spiderqueen.core.SpiderQueen;
@@ -33,14 +35,37 @@ public class ServerTickHandler
 	private int timeUntilSpawnWeb = Time.MINUTE * 1;
 	public List<PlayerEatEntry> playersEating = new ArrayList<PlayerEatEntry>();
 	private List<PlayerEatEntry> playersNoLongerEating = new ArrayList<PlayerEatEntry>();
-	
+
 	/**
 	 * Fires once per tick in-game.
 	 */
 	public void onTick()
 	{
 		timeUntilSpawnWeb--;
-		
+
+		for (final WorldServer worldServer : MinecraftServer.getServer().worldServers)
+		{
+			for (Object obj : worldServer.playerEntities)
+			{
+				EntityPlayer player = (EntityPlayer)obj;
+
+				if (worldServer.isDaytime())
+				{
+					if (player.getActivePotionEffect(Potion.nightVision) != null)
+					{
+						player.removePotionEffect(Potion.nightVision.id);
+					}
+				}
+
+				else
+				{
+					if (player.getActivePotionEffect(Potion.nightVision) == null)
+					{
+						player.addPotionEffect(new PotionEffect(Potion.nightVision.id, 12000, 1, true));
+					}
+				}
+			}
+		}
 		if (timeUntilSpawnWeb <= 0)
 		{
 			for (final WorldServer worldServer : MinecraftServer.getServer().worldServers)
@@ -51,30 +76,30 @@ public class ServerTickHandler
 					player.inventory.addItemStackToInventory(new ItemStack(SpiderQueen.getInstance().itemWeb, LogicHelper.getNumberInRange(1, 3)));
 				}
 			}
-			
+
 			timeUntilSpawnWeb = Time.MINUTE * 1;
 		}
-		
+
 		for (PlayerEatEntry eatEntry : playersEating)
 		{
 			//Check if the player is still on the same slot.
 			if (eatEntry.getPlayer().inventory.currentItem == eatEntry.getSlot())
 			{
 				final ItemStack currentItemStack = eatEntry.getPlayer().getCurrentEquippedItem();
-				
+
 				if (currentItemStack == null || (currentItemStack.stackSize < eatEntry.getCount() && currentItemStack.getItem() instanceof ItemFood))
 				{
 					eatEntry.getPlayer().inventory.addItemStackToInventory(new ItemStack(SpiderQueen.getInstance().itemWeb, LogicHelper.getNumberInRange(1, 3)));
 					playersNoLongerEating.add(eatEntry);
 				}
 			}
-			
+
 			else //They've moved to a different slot. Ignore.
 			{
 				playersNoLongerEating.add(eatEntry);
 			}
 		}
-		
+
 		//Empty the playersEating list.
 		for (PlayerEatEntry eatEntry : playersNoLongerEating)
 		{
