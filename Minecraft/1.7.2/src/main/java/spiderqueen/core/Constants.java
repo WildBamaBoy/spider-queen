351ce6d13731555261c81cf00f32eec1b324aa55
WildBamaBoy <dustya@otelco.net>
2014-05-13 14:20:25 -0500

    Up version to beta

--- Minecraft/1.7.2/src/main/java/spiderqueen/core/Constants.java
+++ Minecraft/1.7.2/src/main/java/spiderqueen/core/Constants.java
@@ -15,7 +15,7 @@ public final class Constants
 	 * Will be beginning version. -0.7.0.Alpha + minor fixes -0.8.0.Beta + minor
 	 * fixes -0.9.0.PreRelease + minor fixes -1.0.0.
 	 */
-	public static final String	VERSION				= "0.7.0.Alpha";
+	public static final String	VERSION				= "0.8.0.Beta";
 
 	public static final String	SKINS_URL			= "http://pastebin.com/raw.php?i=L5S632xR";
 
