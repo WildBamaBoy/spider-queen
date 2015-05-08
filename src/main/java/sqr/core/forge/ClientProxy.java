d05e5e6c46303f39491b757136da9ea32a96f469
WildBamaBoy <wildbamaboy@gmail.com>
2015-05-08 00:28:04 -0500

    Import spider queen model into game.

--- src/main/java/sqr/core/forge/ClientProxy.java
+++ src/main/java/sqr/core/forge/ClientProxy.java
@@ -1,6 +1,19 @@
 package sqr.core.forge;
 
+import net.minecraft.entity.player.EntityPlayer;
+import sqr.client.render.RenderSpiderQueen;
+import cpw.mods.fml.client.registry.RenderingRegistry;
+
 public final class ClientProxy extends ServerProxy
 {
-	
+	@Override
+	public void registerRenderers() 
+	{
+		RenderingRegistry.registerEntityRenderingHandler(EntityPlayer.class, new RenderSpiderQueen());
+	}
+
+	@Override
+	public void registerEventHandlers() 
+	{
+	}
 }
