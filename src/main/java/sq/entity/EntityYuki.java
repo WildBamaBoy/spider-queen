38b7bde371fab5d231294a7255c3173d942b4f47
WildBamaBoy <wildbamaboy@gmail.com>
2015-05-17 15:43:36 -0500

    Add first concept of Yuki

--- src/main/java/sq/entity/EntityYuki.java
+++ src/main/java/sq/entity/EntityYuki.java
@@ -1,9 +1,15 @@
 package sq.entity;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.effect.EntityLightningBolt;
 import net.minecraft.world.World;
+import radixcore.constant.Time;
+import radixcore.util.RadixMath;
 
 public class EntityYuki extends AbstractFlyingMob
 {
+	private int lightningTimer;
+
 	public EntityYuki(World world) 
 	{
 		super(world, "yuki");
@@ -26,11 +32,43 @@ public class EntityYuki extends AbstractFlyingMob
 	{
 		return 0.75F;
 	}
-	
+
 	@Override
 	public boolean isPassive()
 	{
 		return false;
+	}
+
+	@Override
+	public boolean isAIEnabled()
+	{
+		return false;
+	}
+
+	@Override
+	public void onUpdate() 
+	{
+		super.onUpdate();
+
+		if (!worldObj.isRemote)
+		{
+			lightningTimer--;
+
+			if (lightningTimer <= 0)
+			{
+				lightningTimer = Time.SECOND * RadixMath.getNumberInRange(3, 5);
+
+				if (this.getEntityToAttack() != null)
+				{
+					int modX = RadixMath.getNumberInRange(-3, 3);
+					int modZ = RadixMath.getNumberInRange(-3, 3);
+
+					Entity entityToAttack = this.getEntityToAttack();
+					EntityLightningBolt lightning = new EntityLightningBolt(worldObj, entityToAttack.posX + modX, entityToAttack.posY, entityToAttack.posZ + modZ);
+					worldObj.spawnEntityInWorld(lightning);
+				}
+			}
+		}
 	}
 }
 
