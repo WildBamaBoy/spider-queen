153898b04fe4c6a8b199e7ccbf595993ecf86163
WildBamaBoy <wildbamaboy@gmail.com>
2015-05-24 14:07:59 -0500

    Begin work on webs

---
+++ src/main/java/sq/entity/EntityWeb.java
@@ -0,0 +1,604 @@
+package sq.entity;
+
+import java.util.List;
+import java.util.Random;
+
+import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.IProjectile;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.MathHelper;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.Vec3;
+import net.minecraft.world.World;
+import radixcore.math.Point3D;
+import radixcore.util.RadixLogic;
+import sq.core.minecraft.ModBlocks;
+import sq.enums.EnumCocoonType;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class EntityWeb extends Entity implements IProjectile
+{
+	private int				ticksInAir;
+	private int				type;
+	private boolean			doBlockSpawn;
+
+	public EntityLivingBase	shooter;
+	public double			accelerationX;
+	public double			accelerationY;
+	public double			accelerationZ;
+
+	public EntityWeb(World worldObj)
+	{
+		super(worldObj);
+	}
+
+	public EntityWeb(EntityPlayer player)
+	{
+		this(player.worldObj);
+
+		final Vec3 vec = player.getLookVec();
+
+		shooter = player;
+		setPosition(player.posX, player.posY + 1.4F, player.posZ);
+		accelerationX = vec.xCoord * 1.5;
+		accelerationY = vec.yCoord * 1.5;
+		accelerationZ = vec.zCoord * 1.5;
+
+		motionX = accelerationX;
+		motionY = accelerationY;
+		motionZ = accelerationZ;
+
+		doBlockSpawn = true;
+	}
+
+	public EntityWeb(EntityPlayer player, int type)
+	{
+		this(player);
+		this.type = type;
+	}
+
+	public EntityWeb(EntityLivingBase shooter, EntityLivingBase target, float speed)
+	{
+		this(shooter.worldObj);
+		this.shooter = shooter;
+		renderDistanceWeight = 10.0D;
+
+		posY = shooter.posY + shooter.getEyeHeight() - 0.10000000149011612D;
+		final double deltaX = target.posX - shooter.posX;
+		final double deltaY = target.boundingBox.minY + target.height / 3.0F - posY;
+		final double deltaZ = target.posZ - shooter.posZ;
+		final double distanceXZ = MathHelper.sqrt_double(deltaX * deltaX + deltaZ * deltaZ);
+
+		if (distanceXZ >= 1.0E-7D)
+		{
+			final float rotationYaw = (float) (Math.atan2(deltaZ, deltaX) * 180.0D / Math.PI) - 90.0F;
+			final float rotationPitch = (float) -(Math.atan2(deltaY, distanceXZ) * 180.0D / Math.PI);
+			final double modX = deltaX / distanceXZ;
+			final double modY = deltaZ / distanceXZ;
+
+			setLocationAndAngles(shooter.posX + modX, posY, shooter.posZ + modY, rotationYaw, rotationPitch);
+			yOffset = 0.0F;
+
+			final float modDeltaY = (float) distanceXZ * 0.2F;
+			setThrowableHeading(deltaX, deltaY - modDeltaY, deltaZ, speed, 16F);
+		}
+	}
+
+	@Override
+	protected void entityInit()
+	{
+		// No init.
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public boolean isInRangeToRenderDist(double distance)
+	{
+		double weightedLength = boundingBox.getAverageEdgeLength() * 4.0D;
+		weightedLength *= 64.0D;
+		return distance < weightedLength * weightedLength;
+	}
+
+	@Override
+	public void onUpdate()
+	{
+		if (!worldObj.isRemote && (ticksInAir > 150 || shooter != null && shooter.isDead || !worldObj.blockExists((int) posX, (int) posY, (int) posZ)))
+		{
+			setDead();
+		}
+
+		else
+		{
+			super.onUpdate();
+
+			if (!worldObj.isRemote)
+			{
+				if (shooter != null && getDistanceToEntity(shooter) > 150.0D)
+				{
+					setDead();
+					return;
+				}
+
+				if (type == 2 && worldObj.getBlock((int) posX, (int) posY, (int) posZ) == Blocks.lava)
+				{
+					for (final Point3D point : RadixLogic.getNearbyBlocks(this, Blocks.lava, 2))
+					{
+						worldObj.setBlock(point.iPosX, point.iPosY, point.iPosZ, Blocks.fire);
+					}
+
+					setDead();
+				}
+			}
+
+			updateCollision();
+			updateMotion();
+		}
+	}
+
+	@Override
+	public void writeEntityToNBT(NBTTagCompound nbt)
+	{
+		// No data to write.
+	}
+
+	@Override
+	public void readEntityFromNBT(NBTTagCompound nbt)
+	{
+		// No data to read.
+	}
+
+	@Override
+	public boolean canBeCollidedWith()
+	{
+		return true;
+	}
+
+	@Override
+	public float getCollisionBorderSize()
+	{
+		return 1.0F;
+	}
+
+	@Override
+	public boolean attackEntityFrom(DamageSource damageSource, float damageAmount)
+	{
+		if (isEntityInvulnerable())
+		{
+			return false;
+		}
+
+		else
+		{
+			setBeenAttacked();
+
+			if (damageSource.getEntity() != null)
+			{
+				final Vec3 sourceLookVector = damageSource.getEntity().getLookVec();
+
+				if (sourceLookVector != null)
+				{
+					motionX = sourceLookVector.xCoord;
+					motionY = sourceLookVector.yCoord;
+					motionZ = sourceLookVector.zCoord;
+					accelerationX = motionX * 0.1D;
+					accelerationY = motionY * 0.1D;
+					accelerationZ = motionZ * 0.1D;
+				}
+
+				return true;
+			}
+
+			else
+			{
+				return false;
+			}
+		}
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public float getShadowSize()
+	{
+		return 0.0F;
+	}
+
+	@Override
+	public float getBrightness(float unknown)
+	{
+		return 1.0F;
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public int getBrightnessForRender(float unknown)
+	{
+		return 15728880;
+	}
+
+	@Override
+	public void setThrowableHeading(double posX, double posY, double posZ, float speed, float unknown)
+	{
+		final float distanceXYZ = MathHelper.sqrt_double(posX * posX + posY * posY + posZ * posZ);
+		posX /= distanceXYZ;
+		posY /= distanceXYZ;
+		posZ /= distanceXYZ;
+
+		posX += rand.nextGaussian() * (rand.nextBoolean() ? -1 : 1) * 0.007499999832361937D * unknown;
+		posY += rand.nextGaussian() * (rand.nextBoolean() ? -1 : 1) * 0.007499999832361937D * unknown;
+		posZ += rand.nextGaussian() * (rand.nextBoolean() ? -1 : 1) * 0.007499999832361937D * unknown;
+
+		posX *= speed;
+		posY *= speed;
+		posZ *= speed;
+
+		motionX = posX;
+		motionZ = posZ;
+
+		final float distanceXZ = MathHelper.sqrt_double(posX * posX + posZ * posZ);
+		prevRotationYaw = rotationYaw = (float) (Math.atan2(posX, posZ) * 180.0D / Math.PI);
+		prevRotationPitch = rotationPitch = (float) (Math.atan2(posY, distanceXZ) * 180.0D / Math.PI);
+	}
+
+	@Override
+	public void setDead()
+	{
+		super.setDead();
+
+		if (type == 2)
+		{
+			for (int i = -2; i < 2; i++)
+			{
+				worldObj.getBlock((int) posX, (int) posY + i, (int) posZ);
+
+				for (final Point3D point : RadixLogic.getNearbyBlocks(this, Blocks.lava, 2))
+				{
+					worldObj.setBlock(point.iPosX, point.iPosY, point.iPosZ, Blocks.fire);
+				}
+			}
+		}
+	}
+
+	private void updateCollision()
+	{
+		Vec3 currentVector = Vec3.createVectorHelper(posX, posY, posZ);
+		Vec3 nextVector = Vec3.createVectorHelper(posX + motionX, posY + motionY, posZ + motionZ);
+		MovingObjectPosition collisionPosition = worldObj.rayTraceBlocks(currentVector, nextVector);
+		currentVector = Vec3.createVectorHelper(posX, posY, posZ);
+		nextVector = Vec3.createVectorHelper(posX + motionX, posY + motionY, posZ + motionZ);
+
+		if (collisionPosition != null)
+		{
+			nextVector = Vec3.createVectorHelper(collisionPosition.hitVec.xCoord, collisionPosition.hitVec.yCoord, collisionPosition.hitVec.zCoord);
+		}
+
+		Entity collidedEntity = null;
+		final List entitiesInAABB = worldObj.getEntitiesWithinAABBExcludingEntity(this, boundingBox.addCoord(motionX, motionY, motionZ).expand(1.0D, 1.0D, 1.0D));
+		double lastDistance = 0.0D;
+
+		for (int counter = 0; counter < entitiesInAABB.size(); ++counter)
+		{
+			final Entity entityInList = (Entity) entitiesInAABB.get(counter);
+
+			if (entityInList.canBeCollidedWith() && (!entityInList.isEntityEqual(shooter) || ticksInAir >= 25))
+			{
+				final AxisAlignedBB AABB = entityInList.boundingBox.expand(0.3D, 0.3D, 0.3D);
+				final MovingObjectPosition entityCollisionPosition = AABB.calculateIntercept(currentVector, nextVector);
+
+				if (entityCollisionPosition != null)
+				{
+					final double thisDistance = currentVector.distanceTo(entityCollisionPosition.hitVec);
+
+					if (thisDistance < lastDistance || lastDistance == 0.0D)
+					{
+						collidedEntity = entityInList;
+						lastDistance = thisDistance;
+					}
+				}
+			}
+		}
+
+		if (collidedEntity != null)
+		{
+			collisionPosition = new MovingObjectPosition(collidedEntity);
+		}
+
+		if (collisionPosition != null)
+		{
+			onImpact(collisionPosition);
+		}
+	}
+
+	private void updateMotion()
+	{
+		ticksInAir++;
+
+		posX += motionX;
+		posY += motionY;
+		posZ += motionZ;
+		final float f1 = MathHelper.sqrt_double(motionX * motionX + motionZ * motionZ);
+		rotationYaw = (float) (Math.atan2(motionZ, motionX) * 180.0D / Math.PI) + 90.0F;
+
+		for (rotationPitch = (float) (Math.atan2(f1, motionY) * 180.0D / Math.PI) - 90.0F; rotationPitch - prevRotationPitch < -180.0F; prevRotationPitch -= 360.0F)
+		{
+			;
+		}
+
+		while (rotationPitch - prevRotationPitch >= 180.0F)
+		{
+			prevRotationPitch += 360.0F;
+		}
+
+		while (rotationYaw - prevRotationYaw < -180.0F)
+		{
+			prevRotationYaw -= 360.0F;
+		}
+
+		while (rotationYaw - prevRotationYaw >= 180.0F)
+		{
+			prevRotationYaw += 360.0F;
+		}
+
+		rotationPitch = prevRotationPitch + (rotationPitch - prevRotationPitch) * 0.2F;
+		rotationYaw = prevRotationYaw + (rotationYaw - prevRotationYaw) * 0.2F;
+		float motionFactor = 0.95F;
+
+		if (isInWater())
+		{
+			for (int counter = 0; counter < 4; ++counter)
+			{
+				final float speedFactor = 0.25F;
+				worldObj.spawnParticle("bubble", posX - motionX * speedFactor, posY - motionY * speedFactor, posZ - motionZ * speedFactor, motionX, motionY, motionZ);
+			}
+
+			motionFactor = 0.8F;
+		}
+
+		motionX += accelerationX;
+		motionY += accelerationY;
+		motionZ += accelerationZ;
+		motionX *= motionFactor;
+		motionY *= motionFactor;
+		motionZ *= motionFactor;
+
+		setPosition(posX, posY, posZ);
+	}
+
+	private void onImpact(MovingObjectPosition impactPoint)
+	{
+		if (!worldObj.isRemote)
+		{
+			if (impactPoint.entityHit != null && impactPoint.entityHit instanceof EntityLivingBase)
+			{
+				final EnumCocoonType cocoonType = EnumCocoonType.getCocoonType(impactPoint.entityHit);
+				final EntityLivingBase entityHit = (EntityLivingBase) impactPoint.entityHit;
+				final float attackPower = type == 1 ? 4.0F : type == 2 ? 0.0F : 0.0F;
+				entityHit.attackEntityFrom(DamageSource.causeMobDamage(shooter), attackPower);
+
+				if (type == 2)
+				{
+					entityHit.setFire(5);
+					setDead();
+				}
+
+				if (cocoonType != null)
+				{
+					if (entityHit.getHealth() > 0.4F)
+					{
+						final Random rand = new Random();
+						final int intHealth = (int) entityHit.getHealth();
+						final int captureDifficulty = cocoonType.getCatchChance();
+
+						if (captureDifficulty != 0 && rand.nextInt(intHealth / captureDifficulty + 1) != 0)
+						{
+							setToInactive();
+							setNoBlockSpawn();
+							return;
+						}
+
+						else
+						{
+							EntityCocoon entityCocoon = null;
+
+							if (type == 0)
+							{
+								entityCocoon = new EntityCocoon(worldObj, cocoonType);
+							}
+
+							if (entityCocoon != null)
+							{
+								entityCocoon.setLocationAndAngles(entityHit.posX, entityHit.posY, entityHit.posZ, entityHit.rotationYaw, entityHit.rotationPitch);
+								worldObj.spawnEntityInWorld(entityCocoon);
+								entityHit.setDead();
+								setDead();
+							}
+						}
+					}
+				}
+			}
+
+			else
+				// Hit a block.
+			{
+				final Block blockHit = worldObj.getBlock(impactPoint.blockX, impactPoint.blockY, impactPoint.blockZ);
+				int impactX = impactPoint.blockX;
+				int impactY = impactPoint.blockY;
+				int impactZ = impactPoint.blockZ;
+
+				if (blockHit != ModBlocks.blockWebSide && blockHit != ModBlocks.blockWebGround && 
+						blockHit != ModBlocks.blockPoisonWebSide && blockHit != ModBlocks.blockPoisonWebGround)
+				{
+					if (blockHit == Blocks.tallgrass && type == 2)
+					{
+						worldObj.setBlock(impactX, impactY, impactZ, Blocks.fire);
+						setDead();
+						return;
+					}
+
+					else if (blockHit == Blocks.tallgrass && type != 2) { return; }
+
+					else if (blockHit == Blocks.snow_layer)
+					{
+						if (doBlockSpawn)
+						{
+							worldObj.setBlock(impactX, impactY, impactZ, ModBlocks.web, 0, 2);
+							return;
+						}
+					}
+
+					if (doBlockSpawn)
+					{
+						switch (impactPoint.sideHit)
+						{
+						case 0:
+							--impactY;
+							break;
+						case 1:
+							++impactY;
+							break;
+						case 2:
+							--impactZ;
+							break;
+						case 3:
+							++impactZ;
+							break;
+						case 4:
+							--impactX;
+							break;
+						case 5:
+							++impactX;
+						}
+
+						if (worldObj.isAirBlock(impactX, impactY, impactZ))
+						{
+							int meta = 0;
+							switch (impactPoint.sideHit)
+							{
+							case 0:
+								meta = 0;
+								break;
+							case 1:
+								meta = -1;
+								break;
+							case 2:
+								meta = 1;
+								break;
+							case 3:
+								meta = 4;
+								break;
+							case 4:
+								meta = 8;
+								break;
+							case 5:
+								meta = 2;
+								break;
+							}
+
+							if (meta == -1)
+							{
+								if (type == 0)
+								{
+									worldObj.setBlock(impactX, impactY, impactZ, SpiderQueen.getInstance().blockWebGround, 0, 2);
+								}
+
+								else if (type == 1)
+								{
+									worldObj.setBlock(impactX, impactY, impactZ, SpiderQueen.getInstance().blockPoisonWebGround, 0, 2);
+								}
+
+								else if (type == 2)
+								{
+									worldObj.setBlock(impactX, impactY, impactZ, SpiderQueen.getInstance().blockFlameWebFull);
+								}
+							}
+
+							else
+							{
+								if (type == 0)
+								{
+									worldObj.setBlock(impactX, impactY, impactZ, SpiderQueen.getInstance().blockWebSide, meta, 2);
+								}
+
+								else if (type == 1)
+								{
+									worldObj.setBlock(impactX, impactY, impactZ, SpiderQueen.getInstance().blockPoisonWebSide, meta, 2);
+								}
+
+								else if (type == 2)
+								{
+									worldObj.setBlock(impactX, impactY, impactZ, SpiderQueen.getInstance().blockFlameWebFull);
+								}
+							}
+						}
+
+						setDead();
+					}
+				}
+
+				else if (blockHit == SpiderQueen.getInstance().blockWebGround || blockHit == SpiderQueen.getInstance().blockWebSide)
+				{
+					if (type == 0)
+					{
+						worldObj.setBlock(impactX, impactY, impactZ, SpiderQueen.getInstance().blockWebFull);
+					}
+
+					else if (type == 1)
+					{
+						worldObj.setBlock(impactX, impactY, impactZ, SpiderQueen.getInstance().blockPoisonWebFull);
+					}
+
+					else if (type == 2)
+					{
+						worldObj.setBlock(impactX, impactY, impactZ, Blocks.fire);
+					}
+				}
+
+				else if (blockHit == SpiderQueen.getInstance().blockPoisonWebGround || blockHit == SpiderQueen.getInstance().blockPoisonWebSide)
+				{
+					if (type == 0)
+					{
+						worldObj.setBlock(impactX, impactY, impactZ, SpiderQueen.getInstance().blockPoisonWebFull);
+					}
+
+					else if (type == 1)
+					{
+						worldObj.setBlock(impactX, impactY, impactZ, SpiderQueen.getInstance().blockPoisonWebFull);
+					}
+
+					else if (type == 2)
+					{
+						worldObj.setBlock(impactX, impactY, impactZ, Blocks.fire);
+					}
+				}
+			}
+		}
+	}
+
+	private void setToInactive()
+	{
+		final Random rand = new Random();
+		if (rand.nextInt(2) == 0)
+		{
+			motionX = motionX * -0.2F;
+		}
+		if (rand.nextInt(2) == 0)
+		{
+			motionY = motionY * -0.2F;
+		}
+		if (rand.nextInt(2) == 0)
+		{
+			motionZ = motionZ * -0.2F;
+		}
+	}
+
+	private void setNoBlockSpawn()
+	{
+		doBlockSpawn = false;
+	}
+}
--- src/main/java/sq/enums/EnumCocoonType.java
+++ src/main/java/sq/enums/EnumCocoonType.java
@@ -2,43 +2,61 @@ package sq.enums;
 
 import java.lang.reflect.Field;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.monster.EntityBlaze;
+import net.minecraft.entity.monster.EntityCreeper;
+import net.minecraft.entity.monster.EntityEnderman;
+import net.minecraft.entity.monster.EntitySkeleton;
+import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.entity.passive.EntityChicken;
+import net.minecraft.entity.passive.EntityCow;
+import net.minecraft.entity.passive.EntityHorse;
+import net.minecraft.entity.passive.EntityPig;
+import net.minecraft.entity.passive.EntitySheep;
+import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.entity.passive.EntityWolf;
 import sq.core.minecraft.ModItems;
+import sq.entity.EntityAnt;
+import sq.entity.EntityBee;
+import sq.entity.EntityWasp;
 import sq.items.ItemCocoon;
 
 public enum EnumCocoonType
 {
-	BLACK_ANT 	(0, 50, EnumCocoonSize.INSECT, EnumSpiderType.NORMAL),
-	COW 		(1, 40, EnumCocoonSize.NORMAL, EnumSpiderType.NORMAL),
-	CREEPER 	(2, 40, EnumCocoonSize.NORMAL, EnumSpiderType.BOOM),
-	GATHERER_BEE (3, 50, EnumCocoonSize.BEE, EnumSpiderType.NORMAL),
-	HUMAN 		(4, 40, EnumCocoonSize.NORMAL, EnumSpiderType.NORMAL),
-	PIG 		(5, 50, EnumCocoonSize.NORMAL, EnumSpiderType.NORMAL),
-	QUEEN_BEE 	(6, 20, EnumCocoonSize.BEE, EnumSpiderType.NORMAL),
-	SHEEP 		(7, 40, EnumCocoonSize.NORMAL, EnumSpiderType.NORMAL),
-	SKELETON 	(8, 40, EnumCocoonSize.NORMAL, EnumSpiderType.SLINGER),
-	WARRIOR_BEE (9, 35, EnumCocoonSize.BEE, EnumSpiderType.NORMAL),
-	WASP 		(10, 35, EnumCocoonSize.INSECT, EnumSpiderType.NORMAL),
-	WOLF 		(11, 50, EnumCocoonSize.NORMAL, EnumSpiderType.NOVA),
-	ZOMBIE 		(12, 40, EnumCocoonSize.NORMAL, EnumSpiderType.TANK),
-	ENDERMAN 	(13, 30, EnumCocoonSize.TALL, EnumSpiderType.ENDER),
-	BLAZE 		(14, 30, EnumCocoonSize.NORMAL, EnumSpiderType.INFERNO),
-	CHICKEN 	(15, 80, EnumCocoonSize.SMALL, EnumSpiderType.NORMAL),
-	VILLAGER 	(16, 80, EnumCocoonSize.TALL, EnumSpiderType.PACK),
-	HORSE 		(17, 35, EnumCocoonSize.NORMAL, EnumSpiderType.RIDER),
-	RED_ANT		(18, 40, EnumCocoonSize.INSECT, EnumSpiderType.NORMAL);
+	BLACK_ANT 	(0, 50, EnumCocoonSize.INSECT, EnumSpiderType.NORMAL, EntityAnt.class),
+	COW 		(1, 40, EnumCocoonSize.NORMAL, EnumSpiderType.NORMAL, EntityCow.class),
+	CREEPER 	(2, 40, EnumCocoonSize.NORMAL, EnumSpiderType.BOOM, EntityCreeper.class),
+	GATHERER_BEE (3, 50, EnumCocoonSize.BEE, EnumSpiderType.NORMAL, EntityBee.class),
+	HUMAN 		(4, 40, EnumCocoonSize.NORMAL, EnumSpiderType.NORMAL, null), //TODO
+	PIG 		(5, 50, EnumCocoonSize.NORMAL, EnumSpiderType.NORMAL, EntityPig.class),
+	QUEEN_BEE 	(6, 20, EnumCocoonSize.BEE, EnumSpiderType.NORMAL, EntityBee.class),
+	SHEEP 		(7, 40, EnumCocoonSize.NORMAL, EnumSpiderType.NORMAL, EntitySheep.class),
+	SKELETON 	(8, 40, EnumCocoonSize.NORMAL, EnumSpiderType.SLINGER, EntitySkeleton.class),
+	WARRIOR_BEE (9, 35, EnumCocoonSize.BEE, EnumSpiderType.NORMAL, EntityBee.class),
+	WASP 		(10, 35, EnumCocoonSize.INSECT, EnumSpiderType.NORMAL, EntityWasp.class),
+	WOLF 		(11, 50, EnumCocoonSize.NORMAL, EnumSpiderType.NOVA, EntityWolf.class),
+	ZOMBIE 		(12, 40, EnumCocoonSize.NORMAL, EnumSpiderType.TANK, EntityZombie.class),
+	ENDERMAN 	(13, 30, EnumCocoonSize.TALL, EnumSpiderType.ENDER, EntityEnderman.class),
+	BLAZE 		(14, 30, EnumCocoonSize.NORMAL, EnumSpiderType.INFERNO, EntityBlaze.class),
+	CHICKEN 	(15, 80, EnumCocoonSize.SMALL, EnumSpiderType.NORMAL, EntityChicken.class),
+	VILLAGER 	(16, 80, EnumCocoonSize.TALL, EnumSpiderType.PACK, EntityVillager.class),
+	HORSE 		(17, 35, EnumCocoonSize.NORMAL, EnumSpiderType.RIDER, EntityHorse.class),
+	RED_ANT		(18, 40, EnumCocoonSize.INSECT, EnumSpiderType.NORMAL, EntityAnt.class);
 	
 	final int id;
 	final int catchChance;
 	final EnumCocoonSize cocoonSize;
 	final EnumSpiderType spiderTypeYield;
 	final String deathSound;
+	final Class captureClass;
 	
-	EnumCocoonType(int id, int catchChance, EnumCocoonSize size, EnumSpiderType spiderType)
+	EnumCocoonType(int id, int catchChance, EnumCocoonSize size, EnumSpiderType spiderType, Class captureClass)
 	{
 		this.id = id;
 		this.catchChance = catchChance;
 		this.cocoonSize = size;
 		this.spiderTypeYield = spiderType;
+		this.captureClass = captureClass;
 		
 		this.deathSound = "mob." + name().toLowerCase() + ".death";
 	}
@@ -106,5 +124,44 @@ public enum EnumCocoonType
 	public String getName()
 	{
 		return name().toLowerCase().replace("_", "");
+	}
+	
+	public static EnumCocoonType getCocoonType(Entity entity)
+	{
+		if (entity instanceof EntityAnt)
+		{
+			EntityAnt ant = (EntityAnt)entity;
+			
+			switch (ant.getAntType())
+			{
+			case RED: return EnumCocoonType.RED_ANT;
+			case BLACK: return EnumCocoonType.BLACK_ANT;
+			}
+		}
+		
+		else if (entity instanceof EntityBee)
+		{
+			EntityBee bee = (EntityBee)entity;
+			
+			switch (bee.getBeeType())
+			{
+			case GATHERER: return EnumCocoonType.GATHERER_BEE;
+			case QUEEN: return EnumCocoonType.QUEEN_BEE;
+			case WARRIOR: return EnumCocoonType.WARRIOR_BEE;
+			}
+		}
+		
+		else
+		{
+			for (EnumCocoonType type : values())
+			{
+				if (type.captureClass.isAssignableFrom(entity.getClass()))
+				{
+					return type;
+				}
+			}
+		}
+		
+		return null;
 	}
 }
