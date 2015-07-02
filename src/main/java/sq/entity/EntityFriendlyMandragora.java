1bc7772639b7527b462a28537ebc5cb726e0c84e
WildBamaBoy <wildbamaboy@gmail.com>
2015-07-02 01:19:25 -0500

    Feature: Friendly mandragora helps nearby crops grow.

--- src/main/java/sq/entity/EntityFriendlyMandragora.java
+++ src/main/java/sq/entity/EntityFriendlyMandragora.java
@@ -2,37 +2,39 @@ package sq.entity;
 
 import java.util.UUID;
 
+import net.minecraft.block.BlockCrops;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.ai.EntityAIAttackOnCollide;
 import net.minecraft.entity.ai.EntityAIHurtByTarget;
 import net.minecraft.entity.ai.EntityAILookIdle;
-import net.minecraft.entity.ai.EntityAIMoveThroughVillage;
 import net.minecraft.entity.ai.EntityAIMoveTowardsRestriction;
-import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
 import net.minecraft.entity.ai.EntityAISwimming;
 import net.minecraft.entity.ai.EntityAIWander;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
-import net.minecraft.entity.monster.EntityZombie;
-import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
+import radixcore.constant.Particle;
+import radixcore.constant.Time;
+import radixcore.math.Point3D;
+import radixcore.util.RadixLogic;
+import radixcore.util.RadixMath;
+import sq.util.Utils;
 
 public class EntityFriendlyMandragora extends EntityMandragora implements IFriendlyEntity
 {
 	private int timeUntilSpeak;
+	private int timeUntilAbility;
 	private UUID friendPlayerUUID;
 	public EntityLivingBase target;
-	
+
 	public EntityFriendlyMandragora(World world)
 	{
 		super(world);
 	}
-	
+
 	public EntityFriendlyMandragora(World world, EntityPlayer friendPlayer)
 	{
 		super(world);
@@ -41,14 +43,14 @@ public class EntityFriendlyMandragora extends EntityMandragora implements IFrien
 		//Clear old task entries.
 		this.tasks.taskEntries.clear();
 		this.targetTasks.taskEntries.clear();
-		
+
 		//Add custom tasks.
-        this.tasks.addTask(0, new EntityAISwimming(this));
-        this.tasks.addTask(3, new EntityAIMoveTowardsRestriction(this, 1.0D));
-        this.tasks.addTask(5, new EntityAIWander(this, 0.55D));
-        this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
-        this.tasks.addTask(7, new EntityAILookIdle(this));
-        this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
+		this.tasks.addTask(0, new EntityAISwimming(this));
+		this.tasks.addTask(3, new EntityAIMoveTowardsRestriction(this, 1.0D));
+		this.tasks.addTask(5, new EntityAIWander(this, 0.55D));
+		this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+		this.tasks.addTask(7, new EntityAILookIdle(this));
+		this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
 	}
 
 	@Override
@@ -56,14 +58,37 @@ public class EntityFriendlyMandragora extends EntityMandragora implements IFrien
 	{
 		super.onUpdate();
 		FriendlyEntityHelper.onUpdate(this);
+
+		if (!worldObj.isRemote)
+		{
+			timeUntilAbility--;
+
+			if (timeUntilAbility <= 0)
+			{
+				timeUntilAbility = Time.MINUTE * RadixMath.getNumberInRange(2, 5);
+
+				for (Point3D point : RadixLogic.getNearbyBlocks(this, BlockCrops.class, 10))
+				{
+					BlockCrops crop = (BlockCrops)worldObj.getBlock(point.iPosX, point.iPosY, point.iPosZ);
+					int metadata = worldObj.getBlockMetadata(point.iPosX, point.iPosY, point.iPosZ);
+
+					if (metadata < 7)
+					{
+						int max = 7 - metadata;
+						worldObj.setBlockMetadataWithNotify(point.iPosX, point.iPosY, point.iPosZ, metadata + RadixMath.getNumberInRange(1, max), 2);
+						Utils.spawnParticlesAroundEntityS(Particle.HAPPY, this, 16);
+					}
+				}
+			}
+		}
 	}
-	
+
 	@Override
 	public double getMoveSpeed() 
 	{
 		return 0.55F;
 	}
-	
+
 	@Override
 	public UUID getFriendPlayerUUID()
 	{
@@ -74,7 +99,7 @@ public class EntityFriendlyMandragora extends EntityMandragora implements IFrien
 	public void writeEntityToNBT(NBTTagCompound nbt) 
 	{
 		super.writeEntityToNBT(nbt);
-		
+
 		nbt.setLong("friendPlayerUUID-lsb", friendPlayerUUID.getLeastSignificantBits());
 		nbt.setLong("friendPlayerUUID-msb", friendPlayerUUID.getMostSignificantBits());
 	}
@@ -83,10 +108,10 @@ public class EntityFriendlyMandragora extends EntityMandragora implements IFrien
 	public void readEntityFromNBT(NBTTagCompound nbt)
 	{
 		super.readEntityFromNBT(nbt);
-		
+
 		friendPlayerUUID = new UUID(nbt.getLong("friendPlayerUUID-msb"), nbt.getLong("friendPlayerUUID-lsb"));
 	}
-	
+
 	@Override
 	public boolean attackEntityFrom(DamageSource damageSource, float damageAmount)
 	{
@@ -105,7 +130,7 @@ public class EntityFriendlyMandragora extends EntityMandragora implements IFrien
 	{
 		return false;
 	}
-	
+
 	@Override
 	public EntityLivingBase getTarget() 
 	{
@@ -123,13 +148,13 @@ public class EntityFriendlyMandragora extends EntityMandragora implements IFrien
 	{
 		return false;
 	}
-	
+
 	@Override
 	public void setFriendPlayerUUID(UUID value) 
 	{
 		friendPlayerUUID = value;
 	}
-	
+
 	@Override
 	public int getTimeUntilSpeak() 
 	{
@@ -141,7 +166,7 @@ public class EntityFriendlyMandragora extends EntityMandragora implements IFrien
 	{
 		timeUntilSpeak = value;
 	}
-	
+
 	@Override
 	public String getSpeakId() 
 	{
